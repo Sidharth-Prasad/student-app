91411d0b4fec723d4ddeb8798b5bc28734daf07c
Sidharth Prasad <sidharth.prasad@corp.edifecs.com>
2017-07-19 12:09:24 -0700

    Updated models for simplicity

---
+++ src/app/form/model/ratings.ts
@@ -0,0 +1,26 @@
+export class ratings {
+
+    constructor(
+        public UW: number,
+        public SeattleU: number,
+        public BellevueCollege: number,
+        public Target: number,
+        public Nordstrom: number,
+        public Costco: number,
+        public Starbucks: number,
+        public PikePlace: number,
+        public WholeFoods: number,
+        public Microsoft: number,
+        public Amazon: number,
+        public Boeing: number,
+        public Rainier: number,
+        public Rattlesnake: number,
+        public LakeWash: number,
+        public MusOfFlight: number,
+        public PacSci: number,
+        public Chinatown: number,
+        public LGBTQ: number,
+        public SeaCann: number,
+        public SeaArt: number
+    ) { }
+}
